6cd99ff7e53efa788c45dd77f1f16f5fdb59999f
AnjaneyuluDaddala <anjaneyuludaddala07@gmail.com>
2024-04-21 21:01:52 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 // import "./index.css";
 // import './index.css';
 // import App from './App';
-// import Bike from './bike';
+// import Bike from './Bike/bike';
 // import Garage from './react1';
 // import Management from './Destructing/dest';
 
